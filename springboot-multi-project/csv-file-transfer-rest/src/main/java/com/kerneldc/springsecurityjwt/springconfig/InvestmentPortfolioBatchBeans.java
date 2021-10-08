4c2b4eb70b711c698a525dc9ad2401a0b327161f
Tarif Halabi <tarif.halabi@gmail.com>
2021-10-07 19:56:06 -0700

    Refactor.

--- springboot-multi-project/csv-file-transfer-rest/src/main/java/com/kerneldc/springsecurityjwt/springconfig/InvestmentPortfolioBatchBeans.java
+++ springboot-multi-project/csv-file-transfer-rest/src/main/java/com/kerneldc/springsecurityjwt/springconfig/InvestmentPortfolioBatchBeans.java
@@ -1,6 +1,5 @@
 package com.kerneldc.springsecurityjwt.springconfig;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,19 +9,17 @@ import com.kerneldc.portfolio.batch.HoldingPricingService;
 import com.kerneldc.portfolio.repository.ExchangeRateRepository;
 import com.kerneldc.portfolio.repository.HoldingRepository;
 import com.kerneldc.portfolio.repository.PositionRepository;
+import com.kerneldc.portfolio.repository.PriceRepository;
 
 @Configuration
 @EnableScheduling
 public class InvestmentPortfolioBatchBeans {
-
-	@Autowired
-	private HoldingRepository holdingRepository;
-	@Autowired
-	private PositionRepository positionRepository;
-	@Autowired
-	private ExchangeRateRepository exchangeRateRepository;
+	
 	@Bean
-	public HoldingPricingService getHoldingPricingService() {
-		return new HoldingPricingService(holdingRepository, positionRepository, new ExchangeRateService(exchangeRateRepository));
+	public HoldingPricingService getHoldingPricingService(HoldingRepository holdingRepository,
+			PositionRepository positionRepository, PriceRepository priceRepository,
+			ExchangeRateRepository exchangeRateRepository) {
+		return new HoldingPricingService(holdingRepository, positionRepository, priceRepository,
+				new ExchangeRateService(exchangeRateRepository));
 	}
 }
