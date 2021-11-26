559537c38af74ca96eb938573a4237cf2bd1e594
Tarif Halabi <tarif.halabi@gmail.com>
2021-11-26 11:04:47 -0500

    .

---
+++ ipm-be/csv-file-transfer-rest/src/main/java/com/kerneldc/ipm/rest/csv/service/enrichment/PositionBeanReferentialEntityEnrichmentStage1.java
@@ -0,0 +1,42 @@
+package com.kerneldc.ipm.rest.csv.service.enrichment;
+
+import org.springframework.stereotype.Component;
+
+import com.kerneldc.common.domain.AbstractEntity;
+import com.kerneldc.common.enums.IEntityEnum;
+import com.kerneldc.ipm.domain.InvestmentPortfolioTableEnum;
+import com.kerneldc.ipm.domain.Position;
+import com.kerneldc.ipm.rest.csv.service.enrichment.BeanReferentialEntityEnrichmentService.BeanReferentialEntityEnrichmentResult;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class PositionBeanReferentialEntityEnrichmentStage1 implements IBeanReferentialEntityEnrichment {
+
+
+	@Override
+	public BeanReferentialEntityEnrichmentResult enrich(BeanReferentialEntityEnrichmentResult beanReferentialEntityEnrichmentResultList) throws EnrichmentException {
+		var beanList = beanReferentialEntityEnrichmentResultList.beanList();
+		for (AbstractEntity bean: beanList) {
+			var position = (Position) bean;
+			
+			position.setTicker(position.getInstrument().getTicker());
+			position.setExchange(position.getInstrument().getExchange());
+			
+			position.setAccountNumber(position.getPortfolio().getAccountNumber());
+			position.setInstitution(position.getPortfolio().getInstitution());
+
+			position.setPriceTimestamp(position.getPrice().getPriceTimestamp());
+
+		}
+		return beanReferentialEntityEnrichmentResultList;
+	}
+
+	@Override
+	public boolean canHandle(IEntityEnum entityEnum, EnrichmentStageEnum enrichmentStageEnum) {
+		return entityEnum.equals(InvestmentPortfolioTableEnum.POSITION)
+				&& enrichmentStageEnum.equals(EnrichmentStageEnum.STAGE_ONE);
+	}
+
+}
--- ipm-be/investment-portfolio-domain/src/main/java/com/kerneldc/ipm/domain/Position.java
+++ ipm-be/investment-portfolio-domain/src/main/java/com/kerneldc/ipm/domain/Position.java
@@ -7,10 +7,13 @@ import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Transient;
 
 import org.apache.commons.lang3.StringUtils;
 
 import com.kerneldc.common.domain.AbstractPersistableEntity;
+import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvDate;
 
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -23,6 +26,7 @@ public class Position extends AbstractPersistableEntity {
 	
 	private static final long serialVersionUID = 1L;
 
+	@CsvBindByName(column = "position_timestamp")
 	@Setter(AccessLevel.NONE)
 	private OffsetDateTime positionSnapshot;
 	@Setter(AccessLevel.NONE)
@@ -33,11 +37,31 @@ public class Position extends AbstractPersistableEntity {
     @ManyToOne
     @JoinColumn(name = "portfolio_id")
 	private Portfolio portfolio;
+	@CsvBindByName
 	private Float quantity;
 	
     @ManyToOne
     @JoinColumn(name = "price_id")
 	private Price price;
+
+	@Transient
+	@CsvBindByName
+	private String ticker;
+	@Transient
+	@CsvBindByName
+	private String exchange;
+
+	@Transient
+	@CsvBindByName
+	private String institution;
+	@Transient
+	@CsvBindByName(column = "account_number")
+	private String accountNumber;
+
+	@Transient
+	@CsvBindByName(column = "price_timestamp")
+	@CsvDate("uuuu-MM-dd'T'HH:mm:ss.SSSZ")
+	private OffsetDateTime priceTimestamp;
 
 	public void setPositionSnapshot(OffsetDateTime positionSnapshot) {
 		this.positionSnapshot = positionSnapshot;
