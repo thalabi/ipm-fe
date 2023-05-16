581f60a38f55ffe23cf1fee9ff9034bc7e0f051b
Tarif Halabi <tarif.halabi@gmail.com>
2023-05-16 19:20:00 -0400

    Disable YahooFinanceTest until service is up.

--- ipm-be/investment-portfolio-batch/src/test/java/com/kerneldc/ipm/batch/YahooFinanceTest.java
+++ ipm-be/investment-portfolio-batch/src/test/java/com/kerneldc/ipm/batch/YahooFinanceTest.java
@@ -2,12 +2,14 @@ package com.kerneldc.ipm.batch;
 
 import java.io.IOException;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import yahoofinance.YahooFinance;
 
 class YahooFinanceTest {
 
+	@Disabled("Disabled until Yahoo Finance service is up.")
 	@Test
 	void test1() throws IOException {
 		var stock = YahooFinance.get("T");
