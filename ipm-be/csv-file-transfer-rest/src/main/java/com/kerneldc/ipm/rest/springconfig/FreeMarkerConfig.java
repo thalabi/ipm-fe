cd51ebb10d38b2d489e52d9dba023e79b135dceb
Tarif Halabi <tarif.halabi@gmail.com>
2023-06-10 17:01:13 -0400

    Setting FreeMarker locale.

--- ipm-be/csv-file-transfer-rest/src/main/java/com/kerneldc/ipm/rest/springconfig/FreeMarkerConfig.java
+++ ipm-be/csv-file-transfer-rest/src/main/java/com/kerneldc/ipm/rest/springconfig/FreeMarkerConfig.java
@@ -1,9 +1,13 @@
 package com.kerneldc.ipm.rest.springconfig;
 
+import java.io.IOException;
+import java.util.Locale;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
 
+import freemarker.template.TemplateException;
 import lombok.extern.slf4j.Slf4j;
 
 @Configuration
@@ -11,8 +15,10 @@ import lombok.extern.slf4j.Slf4j;
 public class FreeMarkerConfig {
 
 	@Bean
-    public FreeMarkerConfigurationFactoryBean getFreeMarkerConfigurationFactoryBean() {
+    public FreeMarkerConfigurationFactoryBean getFreeMarkerConfigurationFactoryBean() throws IOException, TemplateException {
         FreeMarkerConfigurationFactoryBean fmConfigFactoryBean = new FreeMarkerConfigurationFactoryBean();
-        LOGGER.debug("In FreeMarkerConfig");
+        var freeMarkerConfigConfig = fmConfigFactoryBean.createConfiguration();
+        freeMarkerConfigConfig.setLocale(Locale.getDefault());
+        LOGGER.info("FreeMarker: Setting locale to {}", Locale.getDefault());
         return fmConfigFactoryBean;
     }}
