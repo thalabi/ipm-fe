c688c8ab2ab3da81ba6d854517e7ebc7a40f35ea
Tarif Halabi <tarif.halabi@gmail.com>
2023-06-12 14:47:32 -0400

    Add comment.

--- ipm-be/investment-portfolio-batch/src/test/java/com/kerneldc/ipm/batch/StockPriceServiceTest.java
+++ ipm-be/investment-portfolio-batch/src/test/java/com/kerneldc/ipm/batch/StockPriceServiceTest.java
@@ -18,6 +18,9 @@ class StockPriceServiceTest {
 
 	@BeforeAll
 	public static void beforeAll() {
+		//
+		// Make user ALPHAVANTAGE_API_KEY is defined in Eclipse and Jenkins
+		//
 		String apiKey = System.getenv("ALPHAVANTAGE_API_KEY");
 		System.out.println("Using ALPHAVANTAGE_API_KEY: " + apiKey);
 		stockPriceService = new StockPriceService(null, apiKey);
