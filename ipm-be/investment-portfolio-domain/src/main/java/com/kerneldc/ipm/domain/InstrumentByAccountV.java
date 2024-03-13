5f41b94be59a7f6b124fdd465e645e96546f3934
Tarif Halabi <tarif.halabi@gmail.com>
2024-03-13 14:25:30 -0400

    Add filterable columns to instrument_by_account_v.

--- ipm-be/investment-portfolio-domain/src/main/java/com/kerneldc/ipm/domain/InstrumentByAccountV.java
+++ ipm-be/investment-portfolio-domain/src/main/java/com/kerneldc/ipm/domain/InstrumentByAccountV.java
@@ -1,13 +1,12 @@
 package com.kerneldc.ipm.domain;
 
-import jakarta.persistence.Entity;
-
 import org.hibernate.annotations.Immutable;
 import org.springframework.data.rest.core.annotation.Description;
 
 import com.kerneldc.common.domain.AbstractImmutableEntity;
 import com.opencsv.bean.CsvBindByName;
 
+import jakarta.persistence.Entity;
 import lombok.Getter;
 
 @Entity(name = "instrument_by_account_v")
@@ -16,18 +15,18 @@ import lombok.Getter;
 public class InstrumentByAccountV extends AbstractImmutableEntity {
 	
 	@CsvBindByName(column = "ticker_exchange")
-	@Description("columnDisplayOrder=1,title=Ticker,sortOrder=1") // sortDirection=desc is also available to specify direction
+	@Description("columnDisplayOrder=1,title=Ticker,sortOrder=1,filterable=true,type=text") // sortDirection=desc is also available to specify direction
 	private String tickerExchange;
-	@CsvBindByName(column = "instrument_name")
+	@CsvBindByName(column = "instrument_name,filterable=true,type=text")
 	@Description("columnDisplayOrder=2")
 	private String instrumentName;
 	@CsvBindByName
 	@Description("columnDisplayOrder=3,title=Qty")
 	private Float quantity;
 	@CsvBindByName(column = "account_number")
-	@Description("columnDisplayOrder=4,title=Acc No,sortOrder=2")
+	@Description("columnDisplayOrder=4,title=Acc No,sortOrder=2,filterable=true,type=text")
 	private String accountNumber;
 	@CsvBindByName(column = "account_name")
-	@Description("columnDisplayOrder=5,title=Acc Name")
+	@Description("columnDisplayOrder=5,title=Acc Name,filterable=true,type=text")
 	private String accountName;
 }
