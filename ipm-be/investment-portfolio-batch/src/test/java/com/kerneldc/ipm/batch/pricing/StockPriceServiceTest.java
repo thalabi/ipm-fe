4080ec1eab1f607337dce71d922b7e21de73e8ee
Tarif Halabi <tarif.halabi@gmail.com>
2024-06-06 18:07:29 -0400

    Add feature to fallback to latest price in case of exception.

--- ipm-be/investment-portfolio-batch/src/test/java/com/kerneldc/ipm/batch/pricing/StockPriceServiceTest.java
+++ ipm-be/investment-portfolio-batch/src/test/java/com/kerneldc/ipm/batch/pricing/StockPriceServiceTest.java
@@ -5,38 +5,77 @@ import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
+import static org.mockito.Mockito.when;
 
+import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.Table;
 import com.kerneldc.common.exception.ApplicationException;
 import com.kerneldc.ipm.batch.pricing.ITradingInstrumentPricingService.PriceQuote;
 import com.kerneldc.ipm.domain.ExchangeEnum;
 import com.kerneldc.ipm.domain.Instrument;
+import com.kerneldc.ipm.domain.Price;
 import com.kerneldc.ipm.domain.instrumentdetail.InstrumentStock;
+import com.kerneldc.ipm.repository.PriceRepository;
 
-class StockPriceServiceTest {
+import lombok.extern.slf4j.Slf4j;
+
+@TestInstance(Lifecycle.PER_CLASS) // to be able to use non-static @BeforeAll and @AfterAll methods
+@ExtendWith(SpringExtension.class)
+@TestPropertySource("classpath:application.properties")
+@Slf4j
+class StockPriceServiceTest extends AbstractBaseTest { // TODO fix to use com.kerneldc.common.AbstractBaseTest
+
+	private static final String TEST_RESOURCES_DIRECTORY = "src/test/resources";
+	private static final String WEB_SERVER_DIRECTORY = TEST_RESOURCES_DIRECTORY + "/web-server";
+	private static final String WEB_SERVER_WINDOWS_EXE = "mongoose_windows.exe";
+	private static final String WEB_SERVER_LINUX_EXE = "mongoose_linux";
+	private Process process;
 
 	private static StockAndEtfPriceService stockAndEtfPriceService;
 
+	@MockBean
+	private PriceRepository priceRepository;
+	
+	@Value("${alphavantage.api.url.template}")
+	private String alphavantageApiUrlTemplate;
+	@Value("${alphavantage.api.key}")
+	private String alphavantageApiKey;
+
 	@BeforeAll
-	public static void beforeAll() {
-		//
-		// Make user ALPHAVANTAGE_API_KEY is defined in Eclipse and Jenkins
-		//
-		String apiKey = System.getenv("ALPHAVANTAGE_API_KEY");
-		System.out.println("Using ALPHAVANTAGE_API_KEY: " + apiKey);
-		stockAndEtfPriceService = new StockAndEtfPriceService(null, apiKey);
+	public void beforeAll() throws IOException {
+		LOGGER.info("beforeAll()");
+		
+		var latestPriceList = List.of(price1, price2);
+		when(priceRepository.findLatestPriceList()).thenReturn(latestPriceList);
+		
+		stockAndEtfPriceService = new StockAndEtfPriceService(priceRepository,
+				alphavantageApiUrlTemplate, alphavantageApiKey);
+		
+		spinupWebServer();
 	}
 
-	@Disabled // temporarily because exceeded 25 quotes per day  
 	@Test
-	void testAlphaVantageQuoteOnce() throws ApplicationException {
+	void testAlphaVantageQuoteOnce_Success(TestInfo testInfo) throws ApplicationException {
+		printTestName(testInfo);
 		var instrument = new Instrument();
 		var instrumentStock = new InstrumentStock();
 		instrumentStock.setInstrument(instrument);
@@ -45,14 +84,27 @@ class StockPriceServiceTest {
 		var priceQuote = stockAndEtfPriceService.alphaVantageQuoteService(instrument, instrumentStock);
 		assertThat(priceQuote.lastPrice(), greaterThan(new BigDecimal("0")));
 	}
-	@Disabled // temporarily because exceeded 25 quotes per day  
+
 	@Test
-	void testAlphaVantageQuoteMultipleTimes() throws ApplicationException {
+	void testQuoteThatFailsAndUsesLatestPrice(TestInfo testInfo) throws ApplicationException {
+		printTestName(testInfo);
+		var instrumentStock = new InstrumentStock();
+		instrumentStock.setInstrument(instrument2);
+		instrumentStock.setExchange(ExchangeEnum.NYSE);
+		var priceQuote = stockAndEtfPriceService.quote(instrument2, instrumentStock);
+		System.out.println(priceQuote);
+		assertThat(priceQuote.lastPrice(), greaterThan(new BigDecimal("0")));
+	}
+
+	@Disabled
+	@Test
+	void testAlphaVantageQuoteMultipleTimes(TestInfo testInfo) throws ApplicationException {
+		printTestName(testInfo);
 		var instrument = new Instrument();
 		var instrumentStock = new InstrumentStock();
 		instrumentStock.setInstrument(instrument);
 
-		PriceQuote priceQuote; 
+		PriceQuote priceQuote;
 //		instrument.setTicker("SENS");
 //		instrumentStock.setExchange(ExchangeEnum.CNSX);
 //		priceQuote = stockAndEtfPriceService.alphaVantageQuoteService(instrument, instrumentStock);
@@ -75,9 +127,10 @@ class StockPriceServiceTest {
 		assertThat(priceQuote.lastPrice(), greaterThan(new BigDecimal("0")));
 	}
 
-	@Disabled
 	@Test
 	void testBhccIsUsingFallbackTable() throws ApplicationException {
+		float bhccFallbackPrice = stockAndEtfPriceService.fallbackPriceLookupTable.get("BHCC", ExchangeEnum.CNSX);
+		
 		var instrument = new Instrument();
 		var instrumentStock = new InstrumentStock();
 		instrumentStock.setInstrument(instrument);
@@ -85,17 +138,55 @@ class StockPriceServiceTest {
 		instrumentStock.setExchange(ExchangeEnum.CNSX);
 		var priceQuote = stockAndEtfPriceService.alphaVantageQuoteService(instrument, instrumentStock);
 		assertThat(priceQuote.lastPrice(), is(not(nullValue())));
+		assertThat(priceQuote.lastPrice(), is(new BigDecimal(Float.toString(bhccFallbackPrice))));
 	}
 
-	@Disabled
-	@Test
-	void testFallbackTableLookup() {
-		Table<String, String, Float> fallbackPriceLookupTable = HashBasedTable.create();
-		
-		fallbackPriceLookupTable.put("SENS", "CNSX", 0.01f);
-		fallbackPriceLookupTable.put("BHCC", "CNSX", 0.025f);
+	private static final Price price1 = new Price();
+	private static final Price price2 = new Price();
+	private static final Instrument instrument1 = new Instrument();
+	private static final Instrument instrument2 = new Instrument();
+	static {
+		instrument1.setId(1L);
+		instrument1.setTicker("BCE");
+		price1.setId(1L);
+		price1.setInstrument(instrument1);
+		price1.setPrice(new BigDecimal("1"));
+		price1.setPriceTimestamp(OffsetDateTime.of(2024, 04, 15, 07, 07, 07, 07, ZoneOffset.ofHours(4)));
 
-		var fallbackPrice = fallbackPriceLookupTable.get("BHCC", ExchangeEnum.CNSX);
-		System.out.println("fallbackPrice: "+fallbackPrice);
+		instrument2.setId(2L);
+		instrument2.setTicker("T");
+		price2.setId(2L);
+		price2.setInstrument(instrument2);
+		price2.setPrice(new BigDecimal("2"));
+		price2.setPriceTimestamp(OffsetDateTime.of(2024, 04, 29, 07, 07, 07, 07, ZoneOffset.UTC));
 	}
+
+	@AfterAll
+	public void afterAll() {
+		LOGGER.info("afterAll()");
+		var pid = process.pid();
+		LOGGER.info("killing web server pid: {}", pid);
+		process.destroy();
+	}
+
+	private void spinupWebServer() throws IOException {
+		var pb = new ProcessBuilder(getOsExecutable());
+		pb.directory(new File(WEB_SERVER_DIRECTORY));
+		pb.redirectErrorStream(true);
+		process = pb.start();
+		var pid = process.pid();
+		LOGGER.info("web server started with pid: {}", pid);
+	}
+
+	private String getOsExecutable() {
+		LOGGER.info("OS: {}", System.getProperty("os.name"));
+		if (StringUtils.containsIgnoreCase(System.getProperty("os.name"), "windows")) {
+			return WEB_SERVER_DIRECTORY + "/" + WEB_SERVER_WINDOWS_EXE;
+		} else {
+			var exe = new File(WEB_SERVER_DIRECTORY + "/" + WEB_SERVER_LINUX_EXE);
+			exe.setExecutable(true);
+			return "./" + WEB_SERVER_LINUX_EXE;
+		}
+	}
+	
 }
