ca66bda1b42af9e9e07b5905308d78c6b0f0c2b1
Tarif Halabi <tarif.halabi@gmail.com>
2024-09-06 18:32:24 -0400

    Proof of concept test component.

--- ipm-be/csv-file-transfer-rest/src/main/java/com/kerneldc/ipm/rest/task/IncrementingIntegerComponent.java
+++ ipm-be/csv-file-transfer-rest/src/main/java/com/kerneldc/ipm/rest/task/IncrementingIntegerComponent.java
@@ -7,7 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 @Slf4j
 public class IncrementingIntegerComponent {
-	private static final ThreadLocal<Integer> integerContext = new ThreadLocal<>();
+	//private static final ThreadLocal<Integer> integerContext = new ThreadLocal<>();
 	private int beanInteger = 0;
 
 	public void taskIncrementingSomeInteger() throws InterruptedException {
@@ -15,16 +15,25 @@ public class IncrementingIntegerComponent {
 		LOGGER.info("Begin ...");
 		LOGGER.info("Current thread name: {}, thread ID: {}", Thread.currentThread().getName(), Thread.currentThread().getId());
 		Thread.sleep(1000);
-		if (integerContext.get() == null) {
-			integerContext.set(1);
-		} else {
-			integerContext.set(integerContext.get()+1);
-		}
+//		if (integerContext.get() == null) {
+//			integerContext.set(1);
+//		} else {
+//			integerContext.set(integerContext.get()+1);
+//		}
 		beanInteger++;
 		methodInteger++;
-		LOGGER.info("integerContext: {}, beanInteger: {}, methodInteger: {}", integerContext.get(), beanInteger, methodInteger);
+		//LOGGER.info("integerContext: {}, beanInteger: {}, methodInteger: {}", integerContext.get(), beanInteger, methodInteger);
+		LOGGER.info("beanInteger: {}, methodInteger: {}", beanInteger, methodInteger);
+		LOGGER.info("Sleeping 5 seconds");
+		
 		Thread.sleep(5000);
-		integerContext.remove();
+
+		beanInteger++;
+		methodInteger++;
+		LOGGER.info("beanInteger: {}, methodInteger: {}", beanInteger, methodInteger);
+
+		
+//		integerContext.remove();
 		LOGGER.info("End ...");
 	}
 
