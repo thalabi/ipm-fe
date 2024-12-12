46e53ffa09b37aa34268dc052e1d6acf7ae0b3b0
Tarif Halabi <tarif.halabi@gmail.com>
2024-12-12 16:57:36 -0500

    New utility.

---
+++ ipm-be/common-services/src/main/java/com/kerneldc/ipm/commonservices/enums/EntityEnumUtilities.java
@@ -0,0 +1,25 @@
+package com.kerneldc.ipm.commonservices.enums;
+
+import com.kerneldc.common.enums.IEntityEnum;
+import com.kerneldc.common.enums.UploadTableEnum;
+import com.kerneldc.ipm.domain.InvestmentPortfolioEntityEnum;
+
+public class EntityEnumUtilities {
+	
+	private EntityEnumUtilities() {
+	    throw new IllegalStateException("Utility class not meat to be instantiated");
+	}
+
+	public static IEntityEnum getEntityEnum(String tableName) {
+		IEntityEnum tEnum;
+		if (UploadTableEnum.valueOf(tableName.toUpperCase()) != null) {
+			tEnum = UploadTableEnum.valueOf(tableName.toUpperCase());
+		} else if (InvestmentPortfolioEntityEnum.valueOf(tableName.toUpperCase()) != null) {
+			tEnum = InvestmentPortfolioEntityEnum.valueOf(tableName.toUpperCase());
+		} else {
+			throw new IllegalArgumentException(String.format("There is no corresponding entity enum for [%s]", tableName));
+		}
+		return tEnum;
+	}
+
+}
