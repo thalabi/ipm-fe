ce8fd9795de824e175027e68c770d76e0d5fd79e
Tarif Halabi <tarif.halabi@gmail.com>
2024-12-29 19:55:41 -0500

    Replace valueOf() check with Guava Enums.getIfPresent. valueof() throws an exception if not found.

--- ipm-be/common-services/src/main/java/com/kerneldc/ipm/commonservices/enums/EntityEnumUtilities.java
+++ ipm-be/common-services/src/main/java/com/kerneldc/ipm/commonservices/enums/EntityEnumUtilities.java
@@ -1,5 +1,6 @@
 package com.kerneldc.ipm.commonservices.enums;
 
+import com.google.common.base.Enums;
 import com.kerneldc.common.enums.IEntityEnum;
 import com.kerneldc.common.enums.UploadTableEnum;
 import com.kerneldc.ipm.domain.InvestmentPortfolioEntityEnum;
@@ -12,9 +13,11 @@ public class EntityEnumUtilities {
 
 	public static IEntityEnum getEntityEnum(String tableName) {
 		IEntityEnum tEnum;
-		if (UploadTableEnum.valueOf(tableName.toUpperCase()) != null) {
+//		if (UploadTableEnum.valueOf(tableName.toUpperCase()) != null) {
+		if (Enums.getIfPresent(UploadTableEnum.class, tableName.toUpperCase()).isPresent()) {
 			tEnum = UploadTableEnum.valueOf(tableName.toUpperCase());
-		} else if (InvestmentPortfolioEntityEnum.valueOf(tableName.toUpperCase()) != null) {
+//		} else if (InvestmentPortfolioEntityEnum.valueOf(tableName.toUpperCase()) != null) {
+		} else if (Enums.getIfPresent(InvestmentPortfolioEntityEnum.class, tableName.toUpperCase()).isPresent()) {
 			tEnum = InvestmentPortfolioEntityEnum.valueOf(tableName.toUpperCase());
 		} else {
 			throw new IllegalArgumentException(String.format("There is no corresponding entity enum for [%s]", tableName));
